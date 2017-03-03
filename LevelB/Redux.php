d794c75aa86eb18c7569d86aa82b9bd615c21f23
wolfestein <degardin.florent@gmail.com>
2017-03-03 16:39:08 +0100

    TRICHE 2 :)

--- LevelB/Redux.php
+++ LevelB/Redux.php
@@ -19,7 +19,19 @@ class Redux
     public function getReductedNumber()
     {
         $result = $this->number;
-        // @TODO
-        return $result;
+        $res = PHP_INT_MAX;
+        $strNum = (string) $result;
+
+        while ($res > 9){
+            $len = strlen($strNum);
+            $res = 0;
+            for ($i = 0; $i < $len; $i++){
+                $res += $strNum[$i];
+
+            }
+        }
+        var_dump($res);
+        die();
+        return $res;
     }
 };
